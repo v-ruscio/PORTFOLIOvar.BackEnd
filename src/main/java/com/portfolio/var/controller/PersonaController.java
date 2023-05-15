38a0cf07584681d802fbc3cff2b539ab0f5e1e4c
v-ruscio <victoriaruscio@gmail.com>
2023-05-15 20:38:28 -0300

    añadido url front

--- src/main/java/com/portfolio/var/controller/PersonaController.java
+++ src/main/java/com/portfolio/var/controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ={"https://portfoliovar-frontend.web.app", "http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
